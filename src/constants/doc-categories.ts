16719a77b4aab52af89cf3636ef1b2d4682a3a33
Rujuu <contact@rujuu.com>
2026-03-28 00:26:22 +0900

    :sparkles: add document category type and keys for improved organization and consistency

---
+++ src/constants/doc-categories.ts
@@ -0,0 +1,14 @@
+export type DocCategory =
+  | "getting-started"
+  | "screens"
+  | "customization"
+  | "faq"
+  | "support";
+
+export const DOC_CATEGORY_KEYS: DocCategory[] = [
+  "getting-started",
+  "screens",
+  "customization",
+  "faq",
+  "support",
+];
